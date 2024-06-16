369176a7f263c9bbb185cb4216551ba311ddd148
Siftal <m.khanmohamadzadeh@gmail.com>
2024-06-16 23:08:04 +0330

    feat: Add initial implementation of writer.py

---
+++ eigensdk/chainio/clients/elcontracts/writer.py
@@ -0,0 +1,149 @@
+import logging
+from typing import Any
+
+from eth_account.signers.local import LocalAccount
+from eth_typing import Address
+from web3 import Web3
+from web3.contract.contract import Contract
+from web3.types import TxReceipt
+
+from eigensdk._types import Operator
+
+from ...utils import send_transaction
+from .reader import ELReader
+
+
+class ELWriter:
+    def __init__(
+        self,
+        slasher: Contract,
+        delegation_manager: Contract,
+        strategy_manager: Contract,
+        strategy_manager_addr: Address,
+        avs_directory: Contract,
+        el_reader: ELReader,
+        logger: logging.Logger,
+        eth_http_client: Web3,
+        pk_wallet: LocalAccount,
+    ):
+        self.slasher: Contract = slasher
+        self.delegation_manager: Contract = delegation_manager
+        self.strategy_manager: Contract = strategy_manager
+        self.strategy_manager_addr: Address = strategy_manager_addr
+        self.avs_directory: Contract = avs_directory
+        self.el_reader: ELReader = el_reader
+        self.logger: logging.Logger = logger
+        self.eth_http_client: Web3 = eth_http_client
+        self.pk_wallet: Any = pk_wallet
+
+    def register_as_operator(self, operator: Operator) -> TxReceipt:
+        self.logger.info(f"Registering operator {operator.address} to EigenLayer")
+        op_details = {
+            "earningsReceiver": Web3.to_checksum_address(
+                operator.earnings_receiver_address
+            ),
+            "stakerOptOutWindowBlocks": operator.staker_opt_out_window_blocks,
+            "delegationApprover": Web3.to_checksum_address(
+                operator.delegation_approver_address
+            ),
+        }
+        func = self.delegation_manager.functions.registerAsOperator(
+            op_details, operator.metadata_url
+        )
+        try:
+            receipt = send_transaction(func, self.pk_wallet, self.eth_http_client)
+        except Exception as e:
+            self.logger.error(e)
+            return None
+
+        self.logger.info(
+            "Transaction successfully included",
+            extra={"txHash": receipt["transactionHash"].hex()},
+        )
+        return receipt
+
+    def update_operator_details(self, operator: Operator) -> TxReceipt:
+        self.logger.info(
+            f"Updating operator details of operator {operator.address} to EigenLayer"
+        )
+        op_details = {
+            "earningsReceiver": Web3.to_checksum_address(
+                operator.earnings_receiver_address
+            ),
+            "delegationApprover": Web3.to_checksum_address(
+                operator.delegation_approver_address
+            ),
+            "stakerOptOutWindowBlocks": operator.staker_opt_out_window_blocks,
+        }
+        func = self.delegation_manager.functions.modifyOperatorDetails(op_details)
+        try:
+            receipt = send_transaction(func, self.pk_wallet, self.eth_http_client)
+        except Exception as e:
+            self.logger.error(e)
+            return None
+
+        self.logger.info(
+            "Successfully updated operator details",
+            extra={
+                "txHash": receipt["transactionHash"].hex(),
+                "operator": operator.address,
+            },
+        )
+
+        func = self.delegation_manager.functions.updateOperatorMetadataURI(
+            operator.metadata_url
+        )
+        try:
+            receipt = send_transaction(func, self.pk_wallet, self.eth_http_client)
+        except Exception as e:
+            self.logger.error(e)
+            return None
+
+        self.logger.info(
+            "Successfully updated operator metadata URI",
+            extra={
+                "txHash": receipt["transactionHash"].hex(),
+                "operator": operator.address,
+            },
+        )
+        return receipt
+
+    def deposit_erc20_into_strategy(
+        self, strategy_addr: Address, amount: int
+    ) -> TxReceipt:
+        self.logger.info(f"Depositing {amount} tokens into strategy {strategy_addr}")
+
+        _, underlying_token_contract, underlying_token_addr = (
+            self.el_reader.get_strategy_and_underlying_erc20_token(strategy_addr)
+        )
+
+        approve_func = underlying_token_contract.functions.approve(
+            self.strategy_manager_addr, amount
+        )
+        try:
+            send_transaction(approve_func, self.pk_wallet, self.eth_http_client)
+        except Exception as e:
+            self.logger.error(e)
+            return None
+
+        deposit_func = self.strategy_manager.functions.depositIntoStrategy(
+            strategy_addr, underlying_token_addr, amount
+        )
+        try:
+            receipt = send_transaction(
+                deposit_func, self.pk_wallet, self.eth_http_client
+            )
+        except Exception as e:
+            self.logger.error(e)
+            return None
+
+        self.logger.info(
+            "Successfully deposited the token into the strategy",
+            extra={
+                "txHash": receipt["transactionHash"].hex(),
+                "strategy": strategy_addr,
+                "token": underlying_token_addr,
+                "amount": amount,
+            },
+        )
+        return receipt
---
+++ eigensdk/chainio/utils.py
@@ -0,0 +1,47 @@
+from typing import List
+
+from eth_account.signers.local import LocalAccount
+from web3 import Web3
+from web3.contract.contract import ContractFunction
+from web3.middleware.geth_poa import geth_poa_middleware
+from web3.types import TxReceipt
+
+
+def nums_to_bytes(nums: List[int]) -> bytes:
+    return "".join(map(chr, nums)).encode()
+
+
+def bytes_to_nums(_bytes: bytes) -> List[int]:
+    return [ord(b) for b in _bytes]
+
+
+def send_transaction(
+    func: ContractFunction, pk_wallet: LocalAccount, eth_http_client: Web3
+) -> TxReceipt:
+    try:
+        eth_http_client.middleware_onion.inject(geth_poa_middleware, layer=0)
+    except Exception:
+        pass
+
+    try:
+        gas_estimate = func.estimate_gas({"from": pk_wallet.address})
+    except Exception as e:
+        raise Exception(f"Gas estimation failed: {e}")
+
+    current_gas_price = eth_http_client.eth.gas_price
+
+    tx = func.build_transaction(
+        {
+            "from": pk_wallet.address,
+            "gas": gas_estimate,
+            "gasPrice": current_gas_price,
+            "nonce": eth_http_client.eth.get_transaction_count(pk_wallet.address),
+            "chainId": eth_http_client.eth.chain_id,
+        }
+    )
+    signed_tx = eth_http_client.eth.account.sign_transaction(
+        tx, private_key=pk_wallet.key
+    )
+    tx_hash = eth_http_client.eth.send_raw_transaction(signed_tx.rawTransaction)
+    receipt = eth_http_client.eth.wait_for_transaction_receipt(tx_hash)
+    return receipt
