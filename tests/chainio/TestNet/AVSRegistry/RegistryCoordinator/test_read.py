9968a85fe7568569981cddc3fee76c34300fa297
if3labs <mail@if3.xyz>
2025-04-18 22:12:52 +0400

    Update Convert G1,G2 methods

--- tests/chainio/TestNet/AVSRegistry/RegistryCoordinator/test_read.py
+++
@@ -1,8 +0,0 @@
-from tests.builder import avs_registry_reader
-
-
-
-def test_get_operator_id():
-    return avs_registry_reader.get_operator_id("0x3D3534BFf2cB9cB174eBF7DF9de6386E881d1792")
-
-print(test_get_operator_id())
