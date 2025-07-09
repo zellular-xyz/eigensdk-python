47ec15ba8ac9b3dd68d22dbc449212ff297c1e03
abramsymons <abramsymons@gmail.com>
2025-07-09 15:08:34 +0330

    remove unused types

--- eigensdk/types_.py
+++ eigensdk/types_.py
@@ -26,40 +26,11 @@ class OperatorPubkeys:
 
 
 @dataclass
-class OperatorInfo:
-    socket: str
-    pub_keys: OperatorPubkeys
-
-
-@dataclass
-class OperatorAvsState:
-    operator_id: bytes
-    operator_info: OperatorInfo
-    stake_per_quorum: dict[int, int]
-    block_number: int
-
-
-@dataclass
-class QuorumAvsState:
-    quorum_number: int
-    total_stake: int
-    agg_pub_key_g1: G1Point
-    block_number: int
-
-
-@dataclass
 class OperatorStateRetrieverCheckSignaturesIndices:
     non_signer_quorum_bitmap_indices: list[int]
     quorum_apk_indices: list[int]
     total_stake_indices: list[int]
     non_signer_stake_indices: list[list[int]]
-
-
-@dataclass
-class SignedTaskResponseDigest:
-    task_response: Any
-    bls_signature: Signature
-    operator_id: int
 
 
 @dataclass
